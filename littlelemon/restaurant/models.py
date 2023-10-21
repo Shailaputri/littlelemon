31ba404f41ce60d5b64eb8f14b9ef187ae7060c7
Pooja Sharma <poojasharma@MyBigMac.local>
2023-10-21 21:08:50 +0530

    added database LittleLemon

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
+class BookingTable(models.Model):
+	name = models.CharField(max_length = 255, null=True, blank=True)
+	no_of_guests = models.IntegerField(null=True, blank=True)
+	bookingdate = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+class Menu(models.Model):
+	title = models.CharField(max_length =255, null=True, blank=True)
+	price = models.DecimalField(max_digits = 10, decimal_places = 2, null=True, blank=True)
+	inventory = models.IntegerField(null=True, blank=True)
+
+
+
+
+'''
+For validations:
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+def validate_even(value):
+    if value % 2 != 0:
+        raise ValidationError(
+            _('%(value)s is not an even number'),
+            params={'value': value},
+        )
+'''
