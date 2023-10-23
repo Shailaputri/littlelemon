cfd7a8f81b35c7b5a11d2124c0bddf0a238befb3
Pooja Sharma <poojasharma@MyBigMac.local>
2023-10-23 23:50:19 +0530

    Added token authentication

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from rest_framework import routers
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
 path('home/', views.home, name='home'),
-path('', views.index, name='index'),
+# path('', views.index, name='index'),
 path('menu/', views.MenuView.as_view(), name='menu'),
 path('menu/<int:pk>', views.SingleMenuView.as_view(), name='single_menu'),
+path('msg/',views.msg, name='msg'),
+path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets, generics
 from . import models, serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def home(request):
@@ -14,6 +16,7 @@ class MenuView(generics.ListCreateAPIView):
 	'''
 	handles the POST and GET method calls.
 	'''
+
 	queryset = models.Menu.objects.all()
 	serializer_class = serializers.MenuSerializer
 
@@ -21,10 +24,20 @@ class SingleMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
 	'''
 	GET, PUT and DELETE method calls.
 	'''
+	
 	queryset = models.Menu.objects.all()
 	serializer_class = serializers.MenuSerializer
 
 
 class BookingViewSet(viewsets.ModelViewSet):
+	#We are securing booking table
+	permission_classes=[IsAuthenticated]
 	queryset = models.BookingTable.objects.all()
 	serializer_class = serializers.BookingTableSerializer
+
+
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+	return HttpResponse({'message: View is protected.'})
